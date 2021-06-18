534ebaddda8d7c02244fa497eed259007f9a238e
Sahej Arora <74057328+SahejArora22@users.noreply.github.com>
2021-06-18 16:52:27 +0530

    Add files via upload

---
+++ Rain.js
@@ -0,0 +1,21 @@
+class Rain{
+    constructor(x,y){
+        var option={
+            restitution:1,
+            friction:0.1
+        }
+        this.rain=Bodies.circle(x,y,5,option)
+        this.radius=5
+        World.add(world,this.rain)
+    }
+    update(){
+        if(this.rain.position.y>height){
+      Matter.Body.setPosition(this.rain,{x:random(0,400),y:random(0,400)})
+        }
+    }
+display(){
+    fill("blue")
+    ellipseMode(RADIUS)
+    ellipse(this.rain.position.x,this.rain.position.y,this.radius,this.radius)
+}
+    }
